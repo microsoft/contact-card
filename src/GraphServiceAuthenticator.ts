85f7dd2f6eb67a020532313ac08c1713bec2800b
Max Shmelev <mshmelev@microsoft.com>
2018-12-21 12:57:44 -0800

    Prevent piling up of auth callbacks

--- src/GraphServiceAuthenticator.ts
+++ src/GraphServiceAuthenticator.ts
@@ -1,6 +1,7 @@
 
 export module GraphServiceAuthenticator {
     let authCallback: () => Promise<string>;
+    let currentAuthPromise: Promise<string> | undefined;
 
 
     export function setAuthCallback(callback: () => Promise<string>): void {
@@ -12,6 +13,14 @@ export module GraphServiceAuthenticator {
         if (!authCallback) {
             throw Error("AthCallback is not set. Call GraphServiceAuthenticator.setAuthCallback() to initialize it");
         }
-        return authCallback();
+
+        if (!currentAuthPromise) {                      // prevent the callback bombarding. We can return a currently awaiting (and not fulfilled/rejected promise) in case there's one already
+            currentAuthPromise = authCallback();
+            currentAuthPromise
+                .then(() => { currentAuthPromise = undefined; })
+                .catch(() => { currentAuthPromise = undefined; });
+        }
+
+        return currentAuthPromise;
     }
 }
