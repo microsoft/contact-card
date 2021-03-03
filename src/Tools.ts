ade2b4616b994f901483f0f95e10d33058e31551
Peter Yang <petyan@microsoft.com>
2021-03-02 18:07:38 -0800

    update snapshots

--- src/Tools.ts
+++ src/Tools.ts
@@ -1,7 +1,6 @@
 import * as React from "react";
-import { Button, BaseButton, LinkBase } from "@fluentui/react";
 
-export function openLink(url: string, e?: React.SyntheticEvent<HTMLAnchorElement | HTMLButtonElement | HTMLElement | HTMLDivElement | BaseButton | Button | LinkBase>) {
+export function openLink(url: string, e?: React.MouseEvent<any>) {
     let target: string | undefined = "_self";   // open in same window by default. This also prevents empty tabs for `mailto:` links in Chrome
 
     if (window.self !== window.top) {           // CSP (Content Security Policy) can prevent URL changing if we're inside iFrame. We let browser to decide where to open the URL
