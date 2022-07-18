31a46d4a9541cd48eb37ecc3410b0b09517d177e
softwareteam24 <cornellfroshsoftwareteam@gmail.com>
2022-07-18 15:08:52 -0700

    Fix imports

--- src/Persona.tsx
+++ src/Persona.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { IPersonaProps as IPersonaPropsOF } from "./Persona";
+import { IPersonaProps as IPersonaPropsOF } from "@fluentui/react";
 import { Persona as PersonaOF, PersonaSize } from "@fluentui/react";
 import { IPersonaProfile, PersonaShowMode, ShowModeFlags } from "./Types";
 import { GraphService } from "./GraphService";
