4c447c60400e0f05d56e1b3a62c86496a9683c92
Harikarall <harikarallapalli_1@outlook.com>
2023-02-16 13:48:13 -0600

    Small fix

--- src/contactCard/Summary.tsx
+++ src/contactCard/Summary.tsx
@@ -45,7 +45,7 @@ function renderContactSummary(profile: IPersonaProfile, onContactDetailsClick: (
                 <span>{profile.officeLocation}</span>
                 <span>&nbsp;{profile.city}</span>
             </div>
-            <ActionButton className="more-details contact-details" onClick={onContactDetailsClick} aria-label="Show more" aria-expanded={true} tabIndex={0}>
+            <ActionButton className="more-details contact-details" onClick={onContactDetailsClick} aria-label="Show more" aria-expanded={true} tabIndex={0} aria-live="polite">
                 Show more
             </ActionButton>
         </li>
