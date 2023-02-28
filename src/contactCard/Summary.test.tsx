597f17f53bbd766de86b32e5d95a254b6ff162a8
Harikarall <harikarallapalli_1@outlook.com>
2023-02-28 15:24:21 -0600

    A11y Added function for focus to remain on showmor

--- src/contactCard/Summary.test.tsx
+++ src/contactCard/Summary.test.tsx
@@ -1,59 +1,315 @@
-import * as React from "react";
-import * as Enzyme from "enzyme";
-import { renderSummary } from "./Summary";
-import { buildProfile } from "../supporting/Profile";
+// Jest Snapshot v1, https://goo.gl/fbAQLP
 
-jest.mock("../Tools");
-import { openLink } from "../Tools";
+exports[`renders Summary w/o any manager 1`] = `
+<div>
+  <ul
+    className="summary"
+    tabIndex={-1}
+  >
+    <li>
+      <CustomizedActionButton
+        className="section-title contact-details-button"
+        onClick={[MockFunction]}
+      >
+        Contact 
+        <Icon
+          className="chevron-icon"
+          iconName="ChevronRight"
+        />
+      </CustomizedActionButton>
+      <div
+        className="contact-row"
+      >
+        <Icon
+          className="contact-icon"
+          iconName="Mail"
+        />
+        <StyledLinkBase
+          aria-label="Email user1@contoso.com"
+          className="contact-link email"
+          href="mailto:user1@contoso.com"
+          onClick={[Function]}
+        >
+          user1@contoso.com
+        </StyledLinkBase>
+      </div>
+      <div
+        className="contact-row"
+      >
+        <Icon
+          className="contact-icon"
+          iconName="Phone"
+        />
+        <StyledLinkBase
+          aria-label="Phone 555-123-4561"
+          className="contact-link business-phone"
+          href="tel:555-123-4561"
+          onClick={[Function]}
+        >
+          555-123-4561
+        </StyledLinkBase>
+      </div>
+      <div
+        className="contact-row"
+      >
+        <Icon
+          className="contact-icon"
+          iconName="POI"
+        />
+        <span>
+          1234-1
+        </span>
+        <span>
+           
+          Redmond
+        </span>
+      </div>
+      <CustomizedActionButton
+        aria-expanded={true}
+        aria-label="Show more"
+        aria-live="polite"
+        className="more-details contact-details"
+        data-focus="button"
+        onClick={[Function]}
+      >
+        Show more Click Here
+      </CustomizedActionButton>
+    </li>
+    <li>
+      <CustomizedActionButton
+        className="more-details org-details"
+        onClick={[MockFunction]}
+      >
+        Show organization
+      </CustomizedActionButton>
+    </li>
+  </ul>
+</div>
+`;
 
+exports[`renders Summary with loading manager 1`] = `
+<div>
+  <ul
+    className="summary"
+    tabIndex={-1}
+  >
+    <li>
+      <CustomizedActionButton
+        className="section-title contact-details-button"
+        onClick={[MockFunction]}
+      >
+        Contact 
+        <Icon
+          className="chevron-icon"
+          iconName="ChevronRight"
+        />
+      </CustomizedActionButton>
+      <div
+        className="contact-row"
+      >
+        <Icon
+          className="contact-icon"
+          iconName="Mail"
+        />
+        <StyledLinkBase
+          aria-label="Email user1@contoso.com"
+          className="contact-link email"
+          href="mailto:user1@contoso.com"
+          onClick={[Function]}
+        >
+          user1@contoso.com
+        </StyledLinkBase>
+      </div>
+      <div
+        className="contact-row"
+      >
+        <Icon
+          className="contact-icon"
+          iconName="Phone"
+        />
+        <StyledLinkBase
+          aria-label="Phone 555-123-4561"
+          className="contact-link business-phone"
+          href="tel:555-123-4561"
+          onClick={[Function]}
+        >
+          555-123-4561
+        </StyledLinkBase>
+      </div>
+      <div
+        className="contact-row"
+      >
+        <Icon
+          className="contact-icon"
+          iconName="POI"
+        />
+        <span>
+          1234-1
+        </span>
+        <span>
+           
+          Redmond
+        </span>
+      </div>
+      <CustomizedActionButton
+        aria-expanded={true}
+        aria-label="Show more"
+        aria-live="polite"
+        className="more-details contact-details"
+        data-focus="button"
+        onClick={[Function]}
+      >
+        Show more Click Here
+      </CustomizedActionButton>
+    </li>
+    <li>
+      <CustomizedActionButton
+        className="section-title org-details-button"
+        onClick={[MockFunction]}
+      >
+        Reports to 
+        <Icon
+          className="chevron-icon"
+          iconName="ChevronRight"
+        />
+      </CustomizedActionButton>
+      <div
+        className="person"
+      >
+        <StyledShimmerBase
+          shimmerElements={
+            Array [
+              Object {
+                "height": 40,
+                "type": 2,
+              },
+              Object {
+                "type": 3,
+                "width": 12,
+              },
+              Object {
+                "type": 1,
+              },
+            ]
+          }
+          width="80%"
+        />
+      </div>
+      <CustomizedActionButton
+        className="more-details org-details"
+        onClick={[MockFunction]}
+      >
+        Show organization
+      </CustomizedActionButton>
+    </li>
+  </ul>
+</div>
+`;
 
-test("renders full Summary", () => {
-    const wrap = Enzyme.shallow(<div>{renderSummary(buildProfile(1), buildProfile(2), false, jest.fn(), jest.fn(), jest.fn())}</div>);
-    expect(wrap).toMatchSnapshot();
-});
-
-
-test("renders Summary with loading manager", () => {
-    const wrap = Enzyme.shallow(<div>{renderSummary(buildProfile(1), undefined, true, jest.fn(), jest.fn(), jest.fn())}</div>);
-    expect(wrap).toMatchSnapshot();
-});
-
-
-test("renders Summary w/o any manager", () => {
-    const wrap = Enzyme.shallow(<div>{renderSummary(buildProfile(1), undefined, false, jest.fn(), jest.fn(), jest.fn())}</div>);
-    expect(wrap).toMatchSnapshot();
-});
-
-
-test("Summary handles clicks", () => {
-    const onContactDetailsClick = jest.fn();
-    const onOrgDetailsClick = jest.fn();
-    const onPersonaClick = jest.fn();
-    const wrap = Enzyme.shallow(<div>{renderSummary(buildProfile(1), buildProfile(2), false, onContactDetailsClick, onOrgDetailsClick, onPersonaClick)}</div>);
-
-    wrap.find(".contact-details-button").simulate("click");
-    expect(onContactDetailsClick).toBeCalledTimes(1);
-
-    onContactDetailsClick.mockReset();
-    wrap.find(".more-details.contact-details").simulate("click");
-    expect(onContactDetailsClick).toBeCalledTimes(1);
-
-    wrap.find(".org-details-button").simulate("click");
-    expect(onOrgDetailsClick).toBeCalledTimes(1);
-
-    onOrgDetailsClick.mockReset();
-    wrap.find(".more-details.org-details").simulate("click");
-    expect(onOrgDetailsClick).toBeCalledTimes(1);
-
-    wrap.find(".person.manager").simulate("click");
-    expect(onPersonaClick).toBeCalledTimes(1);
-    expect(onPersonaClick.mock.calls[0][0]).toEqual(buildProfile(2));
-});
-
-
-test("Summary opens links", () => {
-    const wrap = Enzyme.shallow(<div>{renderSummary(buildProfile(1), buildProfile(2), false, jest.fn(), jest.fn(), jest.fn())}</div>);
-    wrap.find(".contact-link.email").simulate("click");
-    wrap.find(".contact-link.business-phone").simulate("click");
-    expect(openLink).toBeCalledTimes(2);
-});
+exports[`renders full Summary 1`] = `
+<div>
+  <ul
+    className="summary"
+    tabIndex={-1}
+  >
+    <li>
+      <CustomizedActionButton
+        className="section-title contact-details-button"
+        onClick={[MockFunction]}
+      >
+        Contact 
+        <Icon
+          className="chevron-icon"
+          iconName="ChevronRight"
+        />
+      </CustomizedActionButton>
+      <div
+        className="contact-row"
+      >
+        <Icon
+          className="contact-icon"
+          iconName="Mail"
+        />
+        <StyledLinkBase
+          aria-label="Email user1@contoso.com"
+          className="contact-link email"
+          href="mailto:user1@contoso.com"
+          onClick={[Function]}
+        >
+          user1@contoso.com
+        </StyledLinkBase>
+      </div>
+      <div
+        className="contact-row"
+      >
+        <Icon
+          className="contact-icon"
+          iconName="Phone"
+        />
+        <StyledLinkBase
+          aria-label="Phone 555-123-4561"
+          className="contact-link business-phone"
+          href="tel:555-123-4561"
+          onClick={[Function]}
+        >
+          555-123-4561
+        </StyledLinkBase>
+      </div>
+      <div
+        className="contact-row"
+      >
+        <Icon
+          className="contact-icon"
+          iconName="POI"
+        />
+        <span>
+          1234-1
+        </span>
+        <span>
+           
+          Redmond
+        </span>
+      </div>
+      <CustomizedActionButton
+        aria-expanded={true}
+        aria-label="Show more"
+        aria-live="polite"
+        className="more-details contact-details"
+        data-focus="button"
+        onClick={[Function]}
+      >
+        Show more Click Here
+      </CustomizedActionButton>
+    </li>
+    <li>
+      <CustomizedActionButton
+        className="section-title org-details-button"
+        onClick={[MockFunction]}
+      >
+        Reports to 
+        <Icon
+          className="chevron-icon"
+          iconName="ChevronRight"
+        />
+      </CustomizedActionButton>
+      <CustomizedActionButton
+        className="person manager"
+        onClick={[Function]}
+      >
+        <Persona
+          displayName="Display Name 2"
+          id="userId2"
+          showMode={3}
+          size={12}
+        />
+      </CustomizedActionButton>
+      <CustomizedActionButton
+        className="more-details org-details"
+        onClick={[MockFunction]}
+      >
+        Show organization
+      </CustomizedActionButton>
+    </li>
+  </ul>
+</div>
+`;
